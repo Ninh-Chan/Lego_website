82087553548490e80a8c64c2d6fab00d5cf66561
Edesaine <132795392+Edesaine@users.noreply.github.com>
2024-03-29 08:47:07 +0700

    New Category view

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,20 +1,68 @@
-<x-app-web-layout>
+<!DOCTYPE html>
+<html lang="en">
 
-    <x-slot name="title">
-        Brands
-    </x-slot>
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="">
+    <meta name="keywords" content="au theme template">
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4> Brands
-                            <a href="{{ url('categories/create') }}" class="btn btn-primary float-end">Add Brands</a>
-                        </h4>
-                    </div>
-                    <div class="card-body">
+    <!-- Title Page-->
+    <title>Dashboard</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- Fontfaces CSS-->
+    <link href="css/php css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+@if (session('status'))
+    <div class="alert alert-success">{{ session('status')}}</div>
+@endif
+
+<div class="page-wrapper">
+
+    <!-- MENU SIDEBAR-->
+
+
+    @include('layouts.sidebar_admin')
+    <!-- END MENU SIDEBAR-->
+
+    <!-- PAGE CONTAINER-->
+    <div class="page-container">
+        <!-- HEADER DESKTOP-->
+        @include('layouts.header_admin')
+        <!-- MAIN CONTENT-->
+        <div class="main-content">
+            <div class="section__content section__content--p30">
+                <div class="container-fluid">
+                    <a href="{{ url('categories/create') }}" class="au-btn au-btn-icon au-btn--blue">
+                        <i style="color: white" class="zmdi zmdi-plus"></i>
+                        <span style="color: white">add brand</span></a>
+                    <br>
+                    <br>
                         <table class="table table-bordered table-striped">
                             <thead>
                             <tr>
@@ -31,7 +79,7 @@
                                         <td>{{$item->id}}</td>
                                         <td>{{$item->name}}</td>
                                         <td>
-                                            <img src="{{ asset($item->image) }}" style="width: 120px; height:80px" >
+                                            <img src="{{ asset($item->image) }}" style="width: 70px; height:70px" alt="">
                                         </td>
                                         <td>
                                             @if ($item->is_active)
@@ -52,11 +100,40 @@
                                 @endforeach
                             </tbody>
                         </table>
-
-                    </div>
                 </div>
+                <!-- END MAIN CONTENT-->
+                <!-- END PAGE CONTAINER-->
             </div>
         </div>
     </div>
+</div>
 
-</x-app-web-layout>
+<!-- Jquery JS-->
+<script src="vendor/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<!-- Vendor JS       -->
+<script src="vendor/slick/slick.min.js">
+</script>
+<script src="vendor/wow/wow.min.js"></script>
+<script src="vendor/animsition/animsition.min.js"></script>
+<script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+</script>
+<script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="vendor/counter-up/jquery.counterup.min.js">
+</script>
+<script src="vendor/circle-progress/circle-progress.min.js"></script>
+<script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="vendor/select2/select2.min.js">
+</script>
+
+<!-- Main JS-->
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
+
+
